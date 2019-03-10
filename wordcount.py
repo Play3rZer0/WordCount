ad848351ed3dc8af3d25fd2bcf1ea83f3c3a4c68
Play3rZer0 <37818562+Play3rZer0@users.noreply.github.com>
2019-03-11 07:06:19 +0800

    Create wordcount.py

---
+++ wordcount.py
@@ -0,0 +1,24 @@
+#Import the libraries into environment
+
+import collections
+import re
+import nltk
+from nltk.corpus import stopwords
+
+#load the stop words list
+
+stop_words = stopwords.words('english')
+
+#open the text file to process
+
+words = re.findall(r'\w+', open('common.txt').read().lower())
+most_common = collections.Counter(words).most_common(100)
+
+#Create an array of words with their occurences 
+
+most_common =  [(word, count) for word, count in most_common if word not in stop_words]
+
+#Print the output to a screen
+
+print ("Here are the results: ")
+print(most_common)
