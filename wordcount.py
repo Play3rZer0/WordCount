73f075d897dc24676b8ca37a6f39dbf3182fadd0
Play3rZer0 <37818562+Play3rZer0@users.noreply.github.com>
2019-03-16 05:11:43 +0800

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -10,17 +10,21 @@ from nltk.corpus import stopwords
 
 stop_words = stopwords.words('english')
 
-#open the text file to process
+#Open the text file to process
 #indicate the name of the file to open
 
-words = re.findall(r'\w+', open('common.txt').read().lower())
+fileName = input("Enter the name of the text file including the extension: ")
+
+#Take the name of the text file and load into memory and process it
+
+words = re.findall(r'\w+', open(fileName).read().lower())
 most_common = collections.Counter(words).most_common(100)
 
-#Create an array of words with their occurences 
+#Create a list of words with their occurences 
 
 most_common =  [(word, count) for word, count in most_common if word not in stop_words]
 
-#Print the output to a screen
+#Print the output to the screen
 
 print ("Here are the results: ")
 print(most_common)
